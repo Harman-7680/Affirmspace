2dc37498b7d3aad32d17ef5685eefa2825cd681b
Harman Singh <harman7680@gmail.com>
2026-03-24 12:19:34 +0530

    Validation applied to bio

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'first_name'   => ['required', 'string', 'max:255'],
             'last_name'    => ['required', 'string', 'max:255'],
-            // 'email'        => [  
+            // 'email'        => [
             //     'required',
             //     'string',
             //     'lowercase',
@@ -25,7 +25,7 @@ class ProfileUpdateRequest extends FormRequest
             //     'max:255',
             //     Rule::unique(User::class)->ignore($this->user()->id),
             // ],
-            'bio'          => ['nullable', 'string'],
+            'bio'          => ['nullable', 'string', 'max:255'],
             'gender'       => [
                 'nullable',
                 'string',
