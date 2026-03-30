7665707940fe4bf940e01917a10173837d318bd3
Harman Singh <harman7680@gmail.com>
2026-03-30 13:48:14 +0530

    Sitemap Updated

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
     @php
         $pages = [
             '/',
@@ -9,7 +8,6 @@
             'refundPolicy',
             'contactWithAdmin',
             'login',
-            'register',
             'terms',
             'blogs',
             'community',
@@ -26,11 +24,18 @@
         </url>
     @endforeach
 
+    <url>
+        <loc>{{ route('register') . '?role=0' }}</loc>
+    </url>
+
+    <url>
+        <loc>{{ route('register') . '?role=1' }}</loc>
+    </url>
+
     @foreach ($blogs as $blog)
         <url>
             <loc>{{ url('/blog/' . $blog->category . '/' . $blog->slug) }}</loc>
             {{-- <lastmod>{{ $blog->updated_at->toAtomString() }}</lastmod> --}}
         </url>
     @endforeach
-
 </urlset>
